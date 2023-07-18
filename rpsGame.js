4c0a7a305228985d3cf877677b14a7050bc073c6
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-07-18 15:56:50 -0500

    Create computer choice function

--- rpsGame.js
+++ rpsGame.js
@@ -1,1 +1,10 @@
-console.log("This works");
+let choices = ["rock", "paper", "scissors"];
+
+function computerChoice (){
+    let randInt = Math.floor(Math.random()* 3);
+    
+    return randInt;
+}
+let test = computerChoice();
+console.log("computerChoice returns " + test);
+console.log("That equates to choosing " + choices[test]);
